d8c956b667b7c8b35344e5e5e456c5745702f717
George Porter <gmporter@cs.ucsd.edu>
2017-10-11 18:09:38 -0700

    Implementations of the client and server

--- TCPWebClient.c
+++ TCPWebClient.c
@@ -37,10 +37,64 @@ int main(int argc, char *argv[]) {
 
   fprintf(stderr, "Connecting to %s to request %s\n", serverStr, queryStr);
 
-  /* DNS lookup code goes here */
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;     // either IPv6 or IPV4 OK
+  hints.ai_socktype = SOCK_STREAM; // use tcp
 
+  if ((rv = getaddrinfo(serverStr, "http", &hints, &servinfo)) != 0) {
+  	DieWithUserMessage("getaddrinfo", gai_strerror(rv));
+  }
 
-  /* Socket code goes here */
+  for (p = servinfo; p != NULL; p = p->ai_next) {
+  	if ((sock = socket(p->ai_family, p->ai_socktype,
+			p->ai_protocol)) == -1) {
+		DieWithUserMessage("socket", "Couldn't connect to address");
+	}
+
+	if (connect(sock, p->ai_addr, p->ai_addrlen) == -1) {
+		DieWithUserMessage("socket", "can't connect");
+	}
+
+	break;
+  }
+
+  if (p == NULL) {
+  	DieWithUserMessage("socket", "Didn't find an address to connect to");
+  }
+  freeaddrinfo(servinfo);
+
+  char addrbuf[128];
+  get_ip_str(p->ai_addr, addrbuf, 128);
+  fprintf(stderr, "Connected to %s!\n", addrbuf);
+
+  // Send the query to the server
+  char query[1024];
+  snprintf(query, 1024, "GET %s HTTP/1.1\r\nHost: %s\r\n\r\n",
+		queryStr, serverStr);
+  fputs(query, stderr);
+
+  ssize_t numBytes = send(sock, query, strlen(query), 0);
+  if (numBytes < 0)
+    DieWithSystemMessage("send() failed");
+
+  fprintf(stderr, "Send the query!\n");
+
+  // Receive the web response
+  do {
+    char buffer[BUFSIZE]; // I/O buffer
+    numBytes = recv(sock, buffer, BUFSIZE - 1, 0);
+    if (numBytes < 0)
+      DieWithSystemMessage("recv() failed");
+    else if (numBytes == 0)
+      break;
+    buffer[numBytes] = '\0';    // Terminate the string!
+    fputs(buffer, stdout);      // Print the echo buffer
+  } while (numBytes > 0);
+
+  fputc('\n', stdout); // Print a final linefeed
+
+  close(sock);
+  fprintf(stderr, "Closed the socket!\n");
 
   exit(0);
 }
--- TCPWebServer.c
+++ TCPWebServer.c
@@ -13,17 +13,42 @@ int main(int argc, char *argv[]) {
 
   char *service = argv[1]; // First arg:  local port
 
-  // Create server socket for incoming connections
+  // Create socket for incoming connections
+  int servSock = SetupTCPServerSocket(service);
+  if (servSock < 0)
+    DieWithUserMessage("SetupTCPServerSocket() failed", service);
 
   for (;;) { // Run forever
-    // Create client socket for incoming connections
+    // New connection creates a connected client socket
+    int clntSock = AcceptTCPConnection(servSock);
 
     HandleTCPClient(clntSock); // Process client
+    close(clntSock);
+    break;
   }
-
-  // Close server socket
+  // NOT REACHED
+  close(servSock);
 }
 
 void HandleTCPClient(int clntSocket) {
-	// handle the actual client socket here
+  char buffer[BUFSIZE]; // Buffer for incoming request
+  memset(buffer, 0, BUFSIZE);
+
+  // Receive part of the request from the client
+  ssize_t numBytesRcvd = recv(clntSocket, buffer, BUFSIZE, 0);
+  if (numBytesRcvd < 0)
+    DieWithSystemMessage("recv() failed");
+
+  // Send received string and receive again until end of stream
+  while (numBytesRcvd > 0) { // 0 indicates end of stream
+  	printf("%s", buffer);
+
+    // See if there is more data to receive
+	memset(buffer, 0, BUFSIZE);
+    numBytesRcvd = recv(clntSocket, buffer, BUFSIZE, 0);
+    if (numBytesRcvd < 0)
+      DieWithSystemMessage("recv() failed");
+  }
+
+  close(clntSocket); // Close client socket
 }
